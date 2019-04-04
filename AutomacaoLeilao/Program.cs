cef1dfca40ccfcec221c294cd03a0fa9de00e330
HelenOliver <helenoliversil@gmail.com>
2019-04-04 13:52:17 -0300

    Adicionar arquivos de projeto.

---
+++ AutomacaoLeilao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+
+namespace AutomacaoLeilao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* IWebDriver firefox = new FirefoxDriver();
+
+            firefox.Navigate().GoToUrl("http://localhost:8080/usuarios/new");*/
+        }
+    }
+}
---
+++ AutomacaoLeilao/Testes/UsuarioSystemTest.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+
+namespace AutomacaoLeilao.Testes
+{
+    [TestFixture]
+    class UsuarioSystemTest
+    {
+        [Test]
+        public void cadastrarUsuario()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://localhost:8080/usuarios/new");
+
+            IWebElement campoNome = driver.FindElement(By.Name("usuario.nome"));
+            IWebElement campoEmail = driver.FindElement(By.Name("usuario.email"));
+            IWebElement btnSalvar = driver.FindElement(By.Id("btnSalvar"));
+
+            campoNome.SendKeys("Helen Oliver");
+            campoEmail.SendKeys("helenoliversil@gmail.com");
+
+            btnSalvar.Click();
+
+            bool achouNome = driver.PageSource.Contains("Helen Oliver");
+            bool achouEmail = driver.PageSource.Contains("helenoliversil@gmail.com");
+
+            Assert.IsTrue(achouNome);
+            Assert.IsTrue(achouEmail);
+
+            //driver.Close();
+
+        }
+
+
+
+    }
+}
