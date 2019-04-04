9426840c0d923d2819c0e1b837394f75a94ac832
HelenOliver <helenoliversil@gmail.com>
2019-04-04 18:43:30 -0300

    Primeira versão do Projeto

---
+++ AutomacaoLeilao/Pages/NovoUsuarioPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+
+namespace AutomacaoLeilao.Pages
+{
+    public class NovoUsuarioPage
+    {
+        IWebDriver driver;
+
+        public NovoUsuarioPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Cadastra(string nome, string email)
+        {
+            IWebElement nomeDoUsuario = driver.FindElement(By.Name("usuario.nome"));
+            IWebElement emailDoUsuario = driver.FindElement(By.Name("usuario.email"));
+
+            nomeDoUsuario.SendKeys(nome);
+            emailDoUsuario.SendKeys(email);
+
+            nomeDoUsuario.Submit();
+        }
+    }
+}
--- AutomacaoLeilao/Testes/UsuarioSystemTest.cs
+++ AutomacaoLeilao/Testes/UsuarioSystemTest.cs
@@ -1,38 +1,28 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using NUnit.Framework;
+using AutomacaoLeilao.Pages;
 
 namespace AutomacaoLeilao.Testes
 {
     [TestFixture]
     class UsuarioSystemTest
     {
+        private UsuarioPage usuarios;
+
+        public UsuarioSystemTest()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            usuarios = new UsuarioPage(driver);
+        }
         [Test]
         public void cadastrarUsuario()
         {
-            IWebDriver driver = new FirefoxDriver();
-            driver.Navigate().GoToUrl("http://localhost:8080/usuarios/new");
+            usuarios.Visita();
+            usuarios.Novo().Cadastra("Helen Oliver", "helenoliversil@gmail.com");
 
-            IWebElement campoNome = driver.FindElement(By.Name("usuario.nome"));
-            IWebElement campoEmail = driver.FindElement(By.Name("usuario.email"));
-            IWebElement btnSalvar = driver.FindElement(By.Id("btnSalvar"));
-
-            campoNome.SendKeys("Helen Oliver");
-            campoEmail.SendKeys("helenoliversil@gmail.com");
-
-            btnSalvar.Click();
-
-            bool achouNome = driver.PageSource.Contains("Helen Oliver");
-            bool achouEmail = driver.PageSource.Contains("helenoliversil@gmail.com");
-
-            Assert.IsTrue(achouNome);
-            Assert.IsTrue(achouEmail);
-
-            //driver.Close();
+            Assert.IsTrue(usuarios.ExisteNaListagem("Helen Oliver", "helenoliversil@gmail.com"));
 
         }
-
-
-
     }
 }
